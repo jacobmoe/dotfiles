da0a8ff33fa655d22b6a483822d5c23603f95fd4
Jacob Moeller <jacobmoe@gmail.com>
2019-06-21 17:42:51 -0400

    call evil mode config

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -489,6 +489,7 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+  (jmoe-evil-mode-config)
   )
 
 ;; ---- my customization ------------------------------------------------------
